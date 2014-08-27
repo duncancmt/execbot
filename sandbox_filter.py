87b8156b70584faef7e8a7afd625055311081ff5
Duncan Townsend <duncant@mit.edu>
2014-08-27 16:57:33 -0400

    Fixed sandbox_filter.py so that it actually imports

--- sandbox_filter.py
+++ sandbox_filter.py
@@ -1,9 +1,10 @@
 def load_constants():
-    import ffi
+    from cffi import FFI
     constants = ['MAP_PRIVATE', 'MAP_FIXED', 'MAP_ANONYMOUS']
     cdef_lines = ""
     for constant in constants:
         cdef_lines += "#define %s ...\n" % constant
+    ffi = FFI()
     ffi.cdef(cdef_lines)
     C = ffi.verify("""
 #include <sys/mman.h>
@@ -12,13 +13,10 @@ def load_constants():
         globals()[constant] = getattr(C, constant)
 load_constants()
 
-from seccomp import *
 from errno import *
-from prctl import *
-
-# not needed becaused the prisoner will jail itself
-#+ SYSCALL('execve', ALLOW)
-#+ SYSCALL('rt_sigreturn', ALLOW)
+from seccomp.bpf import *
+from seccomp.seccomp import *
+from seccomp.prctl import *
 
 filter = compile(  VALIDATE_ARCH
                  + LOAD_SYSCALL_NR
@@ -37,20 +35,15 @@ filter = compile(  VALIDATE_ARCH
                  + SYSCALL('munmap', ALLOW)
                  + SYSCALL('close', ALLOW)
                  + SYSCALL('prctl', # only allow prctl(PR_SET_NO_NEW_PRIVS, 1)
-                             ARG(0),
-                           + JEQ_IMM(prctl.PR_SET_NO_NEW_PRIVS,
-                                       ARG(1),
+                             ARG(0)
+                           + JEQ_IMM(PR_SET_NO_NEW_PRIVS,
+                                       ARG(1)
                                      + JEQ_IMM(1,
                                                ALLOW))
                            + DENY_KILL)
                  # additions that make me sad
                  + SYSCALL('uname', ALLOW)
                  + SYSCALL('access', DENY_ERROR(ENOENT))
-                 + SYSCALL('mprotect',
-                             ARG(2)
-                           + JSET_IMM(PROT_WRITE,
-                                      DENY_KILL)
-                           + ALLOW)
                  # catchall
                  + DENY_KILL)
 
