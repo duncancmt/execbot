6774d8d248a179819260100eef92fa29218df7cd
Duncan Townsend <duncancmt@duncancmt.com>
2015-05-26 14:44:05 -0400

    Use pid.py in jail.py

--- sandbox/jail.py
+++ sandbox/jail.py
@@ -1,4 +1,5 @@
 import sys, os, subprocess
+from pid import PidProc
 from rpython.translator.sandbox.sandlib import VirtualizedSandboxedProc
 from sandbox.pipe import PipeProc
 from rpython.translator.sandbox.vfs import Dir, RealDir, RealFile
@@ -14,15 +15,16 @@ def go_to_jail(chroot_path, jail_uid, jail_gid):
     os.setgid(jail_gid)
     os.setuid(jail_uid)
 
-class JailedProc(VirtualizedSandboxedProc, PipeProc):
-    def __init__(self, sandbox_args, sandbox_executable, tmpdir=None, chroot=None, procdict=None):
+class JailedProc(PidProc, VirtualizedSandboxedProc, PipeProc):
+    def __init__(self, sandbox_args, sandbox_executable, tmpdir=None, chroot=None, procdir=None, p_table=None):
         if '-S' not in self.sandbox_args:
             sandbox_args = ('-S',) + tuple(self.sandbox_args)
 
         sandbox_executable = os.path.abspath(sandbox_executable)
         self.tmpdir = os.path.abspath(tmpdir) if tmpdir is not None else None
         self.chroot = os.path.abspath(chroot) if chroot is not None else None
-        self.procdict = procdict
+        self.procdir = procdir
+        self.p_table = p_table
         self.virtual_root = self.build_virtual_root()
         self.open_fds = {}
 
@@ -44,10 +46,6 @@ class JailedProc(VirtualizedSandboxedProc, PipeProc):
             tmpdirnode = Dir({})
         else:
             tmpdirnode = RealDir(self.tmpdir, exclude=exclude)
-        if self.procdict is None:
-            procdirnode = Dir({})
-        else:
-            procdirnode = Dir(self.procdict)
         libroot = str(LIB_ROOT)
 
         return Dir({
@@ -63,7 +61,7 @@ class JailedProc(VirtualizedSandboxedProc, PipeProc):
                                       exclude=exclude),
                 }),
              'tmp': tmpdirnode,
-             'proc': procdirnode,
+             'proc': procdirnode if procdirnode is not None else Dir({}),
              })
 
 __all__ = ["JailedProc"]
