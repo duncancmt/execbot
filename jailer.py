292685078f168c63c28997c8ee450a4582ec80da
Duncan Townsend <duncancmt@duncancmt.com>
2015-05-26 16:09:03 -0400

    Added jailer.py, the module for manipulating jailed python processes

---
+++ jailer.py
@@ -0,0 +1,65 @@
+import urllib2
+
+from rpython.translator.sandbox.vfs import Dir, File
+
+from sandbox.jail import JailedProc
+
+MAX_HEAP = 16777216
+TMP_DIR = '/execbot/tmp'
+CHROOT_DIR = '/execbot/chroot'
+JAIL_SIZE = 16
+
+prisoners = {}
+procdir = Dir({})
+
+
+def clean_jail():
+    to_delete = set()
+    for i, prisoner in prisoners.iteritems():
+        if prisoner.poll() is not None:
+            to_delete.add(i)
+    for i in to_delete:
+        del prisoners[i]
+        del procdir.entries[str(i)]
+
+
+def jailed_script(source):
+    try:
+        response = urllib2.urlopen(source)
+        content = response.read()
+    except:
+        raise ValueError("Could not download script")
+
+    jailed_expression(content)
+
+def jailed_expression(expr):
+    clean_jail()
+
+    if len(prisoners) >= JAIL_SIZE:
+        raise ValueError("Jail is full")
+
+
+    args = ['-c', expr]
+    exe = '/usr/bin/pypy-c-sandbox'
+    for i in xrange(JAIL_SIZE):
+        if i not in prisoners:
+            pid = i
+    new = JailedProc(args, exe,
+                     tmpdir=TMP_DIR, chroot=CHROOT_DIR,
+                     procdir=procdir, p_table=prisoners)
+    prisoners[pid] = new
+    procdir.entries[str(pid)] = Dir({"source":File(source)})
+
+    clean_jail()
+    
+
+
+def feed_prisoners(food):
+    clean_jail()
+    for prisoner in prisoners.itervalues():
+        prisoner.put(food)
+    poop = {}
+    for i, prisoner in prisoners.iteritems():
+        poop[i] = prisoner.get()
+    clean_jail()
+    return poop
